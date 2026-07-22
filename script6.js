63c75e8cf0697dab16f7f8ba656afe8f49b4c3cb
jrrddev <steroid.be.like@gmail.com>
2026-07-22 20:41:24 +0800

    update

--- script6.js
+++ script6.js
@@ -6,3 +6,36 @@ console.log(num); // 7
 num = 5;
 num += 2;
 console.log(num); // 7
+
+//boolean
+let isOldEnoughToDrive = true;
+if (isOldEnoughToDrive) {
+    console.log("You're old enough to drive"); // You're old enough to drive
+} else {
+    console.log("Sorry, you are not old enough to drive");
+}
+//example2
+let psswd = false;
+if (psswd) {
+    console.log("correct login");
+} else {
+    console.log("wrong password");
+}
+
+//to compare 2 values use this
+console.log(5 == "5");
+//more examples
+console.log(5 == 5); // true
+console.log(10 > 3); // true
+let score = 5;
+console.log(score == 5); // true
+let name = "Alex";
+console.log(name == "Alex"); // true
+
+//for strict equality heres the example ===
+console.log(5 === '5'); // false
+/*
+5 === "5" // false (because Number is not String)
+5 === 5   // true
+ */
+
---
+++ script7.js
@@ -0,0 +1,42 @@
+//What Are Conditional Statements, and How Do If/Else If/Else Statements Work?
+//true and false, the false is either empty string or in integer is 0 the true is opposite
+//example
+if (null) {
+    console.log("this will not run");
+}
+
+if ("wassup") {
+    console.log("this will run");
+}
+//another example
+let age = 20;
+if (age >= 18) {
+    console.log("youre eligible to vote");
+}
+//next example lets add else
+age = 17;
+if (age >= 18) {
+    console.log("youre eligible to vote");
+} else {
+    console.log("youre not eligible to vote");
+}
+//for multiple if else conditions
+let score = 90;
+ if (score >= 90) {
+     console.log("you got an A");
+}
+ else if (score >= 80) {
+     console.log ("you got a B");
+ }
+ else if (score >= 70) {
+     console.log ("you got a C");
+ }
+ else {
+     console.log ("you failed");
+ }
+\
+ //ternary operator
+//basic syntax logic --- condition ? expressionIfTrue : expressionIfFalse;
+const temperature = 30;
+ const weather = temperature > 25 ? "sunny" : "cool";
+ console.log(`its a ${weather} day`);
