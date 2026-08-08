d626da5a6a891bc6a646aeea57083d4a67ddf84c
jrrddev <steroid.be.like@gmail.com>
2026-08-08 13:39:56 +0800

    functions

---
+++ Functions/script14.js
@@ -0,0 +1,7 @@
+//Functions are reusable pieces of code that perform a specific task or calculate a value.
+//Think of functions as a machine that takes some input, does some operations on it, and then produces an output
+function greet() {
+    console.log("Hello World!");
+}
+//to call the function
+greet(); //Hello World!
---
+++ test.js
@@ -0,0 +1,6 @@
+const a = 2;
+if (1 == "1") {
+    let b = 3;
+    console.log(a + b);
+}
+console.log(b);
